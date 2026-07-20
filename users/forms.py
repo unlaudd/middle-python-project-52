e1a0c91de4f9a9be39b2319526447d91e1a4eb63
Denis Lysenko <un1audd@yandex.ru>
2026-07-20 19:15:30 +0300

    fixes

--- users/forms.py
+++ users/forms.py
@@ -11,85 +11,43 @@ class UserRegistrationForm(UserCreationForm):
     """
     Form for registering a new user with first name, last name, and username.
     """
-    first_name = forms.CharField(
-        label=_('Имя'),
-        max_length=150,
-        required=True
-    )
-    last_name = forms.CharField(
-        label=_('Фамилия'),
-        max_length=150,
-        required=True
-    )
-    username = forms.CharField(
-        label=_('Имя пользователя'),
-        max_length=150,
-        required=True
-    )
-    password1 = forms.CharField(
-        label=_('Пароль'),
-        widget=forms.PasswordInput,
-        required=True
-    )
-    password2 = forms.CharField(
-        label=_('Подтверждение пароля'),
-        widget=forms.PasswordInput,
-        required=True,
-        help_text=_('Введите тот же пароль, что и ранее, для проверки.')
-    )
+    first_name = forms.CharField(label=_('Имя'), max_length=150, required=True)
+    last_name = forms.CharField(label=_('Фамилия'), max_length=150, required=True)
 
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name')
 
-    def save(self, commit=True):
-        """
-        Save the user with first name and last name from the form data.
-        """
-        user = super().save(commit=False)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        if commit:
-            user.save()
-        return user
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Переопределяем метки встроенных полей, не ломая их валидацию
+        self.fields['username'].label = _('Имя пользователя')
+        self.fields['password1'].label = _('Пароль')
+        self.fields['password2'].label = _('Подтверждение пароля')
+        self.fields['password2'].help_text = _('Введите тот же пароль, что и ранее, для проверки.')
 
 
 class UserUpdateForm(forms.ModelForm):
     """
     Form for updating user profile information.
     """
-    first_name = forms.CharField(
-        label=_('Имя'),
-        max_length=150,
-        required=True
-    )
-    last_name = forms.CharField(
-        label=_('Фамилия'),
-        max_length=150,
-        required=True
-    )
-    username = forms.CharField(
-        label=_('Имя пользователя'),
-        max_length=150,
-        required=True
-    )
+    first_name = forms.CharField(label=_('Имя'), max_length=150, required=True)
+    last_name = forms.CharField(label=_('Фамилия'), max_length=150, required=True)
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username')
+        fields = ('username', 'first_name', 'last_name')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = _('Имя пользователя')
 
 
 class LoginForm(AuthenticationForm):
     """
     Form for user authentication.
     """
-    username = forms.CharField(
-        label=_('Имя пользователя'),
-        max_length=150,
-        required=True
-    )
-    password = forms.CharField(
-        label=_('Пароль'),
-        widget=forms.PasswordInput,
-        required=True
-    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = _('Имя пользователя')
+        self.fields['password'].label = _('Пароль')
