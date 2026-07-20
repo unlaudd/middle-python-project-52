ff17de82519113c3e11882c42da4faadccd0268f
Denis Lysenko <un1audd@yandex.ru>
2026-07-20 20:14:26 +0300

    fixes

--- users/views.py
+++ users/views.py
@@ -54,7 +54,7 @@ class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('home')
 
     def dispatch(self, request, *args, **kwargs):
-        messages.info(request, _('Вы вышли из системы'))
+        messages.info(request, _('Вы разлогинены'))
         return super().dispatch(request, *args, **kwargs)
 
 
